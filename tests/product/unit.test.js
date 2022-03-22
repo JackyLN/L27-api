f1358ab957b16b18c4411b5a33a7398bf76fa06a
Jacky Le <lenghia1991@gmail.com>
2022-03-22 20:28:14 +0700

    dummy unit test

---
+++ tests/product/unit.test.js
@@ -0,0 +1,8 @@
+const { expect } = require("chai");
+
+describe('getAllProduct', () => {
+  it('should get all product in database', async () => {
+    
+    expect(1).to.be.equal(1); //dummy
+  }); 
+});
